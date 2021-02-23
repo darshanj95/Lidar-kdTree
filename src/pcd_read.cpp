712fbf2abb6096cfa1dd5faf6acbae39664ff37d
darshanj95 <darshan95jain@gmail.com>
2021-02-22 23:53:23 -0500

    added comments

--- src/pcd_read.cpp
+++ src/pcd_read.cpp
@@ -7,34 +7,46 @@ int main(int arg, char ** argv){
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::visualization::PCLVisualizer viewer ("Viewer");
 
-    float filterRes = 0.2;
-    Eigen::Vector4f minPoint(-20, -8, -5, 1);
-    Eigen::Vector4f maxPoint(40, 8, 5, 1);
-    int maxIter = 150;
-    float distanceThreshold = 0.2;
-
+    //Load .pcd file
     if(pcl::io::loadPCDFile<pcl::PointXYZ> ("../data/data0.pcd", *cloud) == -1) {
         PCL_ERROR("Could not load file \n");
         return -1;
     }
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr filterCloud = processor->FilterCloud(cloud, filterRes, minPoint, maxPoint);
-    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = processor->SegmentPlane(filterCloud, maxIter, distanceThreshold);
-
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> colorG(segmentCloud.second,0,255,0);
-
     std::cout <<"LOADED: " << cloud->width * cloud->height << " data points. \n";
 
+    // preprocessing filter paramenter
+    float filterRes = 0.2;
+    Eigen::Vector4f minPoint(-20, -8, -5, 1);
+    Eigen::Vector4f maxPoint(40, 8, 5, 1);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filterCloud = processor->FilterCloud(cloud, filterRes, minPoint, maxPoint);
+
+    //RANSAC Parameter
+    int maxIter = 150;
+    float distanceThreshold = 0.2;
+
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = processor->SegmentPlane(filterCloud, maxIter, distanceThreshold);
+
+    //Clustering Parameter
+    float clusterTolerance = 0.4;
+    int minSize = 100;
+    int maxSize = 500;
+
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = processor->Clustering(segmentCloud.first, clusterTolerance, minSize, maxSize);
+
+    // Visualize
+    //Road
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> colorG(segmentCloud.second,0,255,0);
     viewer.addPointCloud(segmentCloud.second, colorG, "planeCloud");
 
-    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = processor->Clustering(segmentCloud.first, 0.4, 100, 500);
+    //Objects | Clusters
     int clusterId = 0;
     int i = 255;
     int k = 0;
 
     for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
     {
-        std::cout << "cluster size ";
         pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> col(cluster,i,0,k);
         viewer.addPointCloud(cluster,col,"clusters"+std::to_string(clusterId));
         if (i == 255) {
@@ -46,6 +58,7 @@ int main(int arg, char ** argv){
         ++clusterId;
     }
 
+    //Viewer
     while(!viewer.wasStopped())
         viewer.spinOnce();
 
