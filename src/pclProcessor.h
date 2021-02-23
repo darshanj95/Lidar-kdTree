17022b5869183cb247ce213be849d59267d05769
darshanj95 <darshan95jain@gmail.com>
2021-02-22 23:42:24 -0500

    removed unused headerfiles

--- src/pclProcessor.h
+++ src/pclProcessor.h
@@ -6,10 +6,8 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/crop_box.h>
-#include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
-#include <pcl/common/transforms.h>
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include "kdtree.h"
